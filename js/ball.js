ab97c4a09b74e7e38ecc7dc69e67648f1cf25501
Ivan Khud <anakin16@gmail.com>
2017-10-03 20:14:35 +0300

    #30 moved ball related code to ball.js

--- js/ball.js
+++ js/ball.js
@@ -3,3 +3,36 @@ function Ball(safe, unsafe, steps) {
   this.steps = steps;
   this.unsafe = unsafe;
 }
+
+function firstThrow(firstStep, floor) {
+  var isFirstBallSafe = true, currentStep = firstStep, currentFloor =0, firstResult = new Ball();
+  firstResult.steps = 0;
+  firstResult.safe = 0;
+  while (isFirstBallSafe && (currentFloor < 99)) {
+    currentFloor += currentStep; 
+    firstResult.steps++;
+    if (currentFloor >= floor) {
+      isFirstBallSafe = false;
+      firstResult.unsafe = currentFloor; 
+    }
+    else {
+      currentStep--;
+      firstResult.safe = currentFloor;
+    }  
+  }
+  return firstResult;
+}
+
+function secondThrow(safe, unsafe, floor) {
+  var currentFloor = safe + 1; var steps = 0;
+  while (currentFloor < unsafe) {
+    steps++; 
+    if (currentFloor === floor) {
+      unsafe = currentFloor;
+    }
+    else {
+      currentFloor++;
+    } 
+  }
+  return steps;
+} 
--- js/main.js
+++ js/main.js
@@ -1,37 +1,3 @@
-function firstThrow(firstStep, floor) {
-  var isFirstBallSafe = true, currentStep = firstStep, currentFloor =0, firstResult = new Ball();
-  firstResult.steps = 0;
-  firstResult.safe = 0;
-  while (isFirstBallSafe && (currentFloor < 99)) {
-    currentFloor += currentStep; 
-    firstResult.steps++;
-    if (currentFloor >= floor) {
-      isFirstBallSafe = false;
-      firstResult.unsafe = currentFloor; 
-    }
-    else {
-      currentStep--;
-      firstResult.safe = currentFloor;
-    }  
-  }
-  return firstResult;
-}
-
-  
-function secondThrow(safe, unsafe, floor) {
-  var currentFloor = safe + 1; var steps = 0;
-  while (currentFloor < unsafe) {
-    steps++; 
-    if (currentFloor === floor) {
-      unsafe = currentFloor;
-    }
-    else {
-      currentFloor++;
-    } 
-  }
-  return steps;
-}    
-    
 function main() {
   dom.results[0] = 0;
   dom.maxRowsPerPage = 5;
