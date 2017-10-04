ddd199c316e30f83c0eed16ac75d57650d24f3af
Ivan Khud <anakin16@gmail.com>
2017-10-04 15:19:49 +0300

    #27 formatted if-else statements

--- js/dom.js
+++ js/dom.js
@@ -32,19 +32,19 @@ var dom = {
      button.innerHTML = 'Reset';
      button.className = 'btn btn-danger btn-block';
      document.getElementById("startMessage").style.visibility = 'hidden';
-    }
-      else {
-        document.getElementById("maxRowsPerPage").value = 5;
-        document.getElementById("maxRowsPerPageForm").style.visibility = 'hidden';
-        document.getElementById("pages").style.visibility = 'hidden';
-        document.getElementById("firstVisibleRowForm").style.visibility = 'hidden';
-        document.getElementById("errorMessage").style.visibility = 'hidden';
-        document.getElementById('generalTable').innerHTML = ""; 
-        document.getElementById('statisticsTable').innerHTML = "";
-        button.innerHTML = 'Start';
-        button.className = 'btn btn-success btn-block';
-        document.getElementById("startMessage").style.visibility = 'visible';
-      };
+    }     
+    else {
+      document.getElementById("maxRowsPerPage").value = 5;
+      document.getElementById("maxRowsPerPageForm").style.visibility = 'hidden';
+      document.getElementById("pages").style.visibility = 'hidden';
+      document.getElementById("firstVisibleRowForm").style.visibility = 'hidden';
+      document.getElementById("errorMessage").style.visibility = 'hidden';
+      document.getElementById('generalTable').innerHTML = ""; 
+      document.getElementById('statisticsTable').innerHTML = "";
+      button.innerHTML = 'Start';
+      button.className = 'btn btn-success btn-block';
+      document.getElementById("startMessage").style.visibility = 'visible';
+    };
     dom.onBodyResize();
   }, 
 
@@ -90,9 +90,9 @@ var dom = {
       dom.lastVisibleRow = 100;
       dom.firstVisibleRow = 101 - dom.maxRowsPerPage;
     }
-      else {
-        dom.lastVisibleRow = dom.firstVisibleRow + dom.maxRowsPerPage - 1;
-      } 
+    else {
+      dom.lastVisibleRow = dom.firstVisibleRow + dom.maxRowsPerPage - 1;
+    } 
     dom.outputResults(dom.firstVisibleRow, dom.lastVisibleRow);
     dom.onBodyResize();
   },
@@ -131,33 +131,33 @@ var dom = {
       document.getElementById("buttonFirst").className = "disabled";
       document.getElementById("buttonPrevious").className = "disabled";
     }
-      else {
-        document.getElementById("buttonFirst").className = "unDisabled";
-        document.getElementById("buttonPrevious").className = "unDisabled";
-      };
+    else {
+      document.getElementById("buttonFirst").className = "unDisabled";
+      document.getElementById("buttonPrevious").className = "unDisabled";
+    };
     if (dom.lastVisibleRow == 100) {
       document.getElementById("buttonNext").className = "disabled";
       document.getElementById("buttonLast").className = "disabled";
     }
-      else {
-      document.getElementById("buttonNext").className = "unDisabled";
-      document.getElementById("buttonLast").className = "unDisabled";
-      };
+    else {
+    document.getElementById("buttonNext").className = "unDisabled";
+    document.getElementById("buttonLast").className = "unDisabled";
+    };
   },
 
   onScrollCheck: function() {
     if (window.pageYOffset > 50) {
       document.getElementById("returnToTop").style.display = "inline";
     }
-      else {
-        document.getElementById("returnToTop").style.display = "none";
-      };
+    else {
+      document.getElementById("returnToTop").style.display = "none";
+    };
     if (window.pageYOffset < (document.body.scrollHeight - document.body.clientHeight - 50)) {
       document.getElementById("returnToBottom").style.display = "inline";
     }
-      else {
-        document.getElementById("returnToBottom").style.display = "none";
-      };
+    else {
+      document.getElementById("returnToBottom").style.display = "none";
+    };
   },
 
   toTop: function() {
@@ -173,11 +173,11 @@ var dom = {
       document.getElementById("topBottomForm").style.display = "inline"; 
       dom.onScrollCheck();
     }
-      else {
-        document.getElementById("returnToBottom").style.display = "none";  
-        document.getElementById("returnToTop").style.display = "none"; 
-        document.getElementById("topBottomForm").style.display = "none"; 
-      };
+    else {
+      document.getElementById("returnToBottom").style.display = "none";  
+      document.getElementById("returnToTop").style.display = "none"; 
+      document.getElementById("topBottomForm").style.display = "none"; 
+    };
   },
 
   firstVisibleRowChange: function() {
@@ -188,28 +188,28 @@ var dom = {
       if ((dom.firstVisibleRow + dom.maxRowsPerPage) > 100) {
         dom.lastVisibleRow = 100;
       }
-        else {
-          dom.lastVisibleRow = dom.firstVisibleRow + dom.maxRowsPerPage - 1;
-        }; 
+      else {
+        dom.lastVisibleRow = dom.firstVisibleRow + dom.maxRowsPerPage - 1;
+      }; 
       dom.outputResults(dom.firstVisibleRow, dom.lastVisibleRow);   
     } 
-      else {
-        document.getElementById("firstVisibleRow").value = "";
-        document.getElementById("errorMessage").style.visibility  = "visible";
-        dom.makeErrorMessage(typedFirstVisibleRow); 
-      }; 
+    else {
+      document.getElementById("firstVisibleRow").value = "";
+      document.getElementById("errorMessage").style.visibility  = "visible";
+      dom.makeErrorMessage(typedFirstVisibleRow); 
+    }; 
   },
 
   makeErrorMessage: function(input) {
     if (input < 1) {
       document.getElementById("errorMessage").innerHTML  = 'You can start from "1" only';
     } 
-      else if (input > 100) {
-        document.getElementById("errorMessage").innerHTML  = 'There are only 100 floors';
-      } 
-        else {
-          document.getElementById("errorMessage").innerHTML  = 'You can only enter a number';
-        };
+    else if (input > 100) {
+      document.getElementById("errorMessage").innerHTML  = 'There are only 100 floors';
+    } 
+    else {
+      document.getElementById("errorMessage").innerHTML  = 'You can only enter a number';
+    };
   }
 
 }
